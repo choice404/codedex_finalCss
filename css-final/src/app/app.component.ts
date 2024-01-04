79563c246c887bb788094be279eb6c4daa0bf741
choisauce <austinch20@protonmail.com>
2024-01-04 00:25:44 -0800

    Only have cursor on name

--- css-final/src/app/app.component.ts
+++ css-final/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Directory } from './interfaces/directory';
 import { Directory2 } from './interfaces/directory2';
 
 @Component({
@@ -143,21 +142,15 @@ export class AppComponent {
 
     fullDescription: string = 'Software Developer, Cybersecurity Enthusiast, Photographer, and so much more!';
 
-    fullDesc: string[] = [
-        'I have always been in love with computers. The first time I ever programmed anything was really unusual... It was in Minecraft using redstone. I learned how to take redstone and create various CPU parts, eventually making a functioning CPU and programming it using assembly. Eventually I took a web development class in school which opened myself to a bigger world of programming. My first website was a simple static site using only HTML, CSS, and a little bit of JavaScript. Today I\'m using technologies that I never knew existed like Angular, TypeScript, React, and so much more.',
-        'I am currently a 2nd year student at UNLV focusing on front-end web development and low level programming. I am also heavily involved in various cybersecurity related organizations at UNLV such as working in the Software Develpment Committee, Internal Affairs Department, and PR department of the <a href=\"https://freecyberclinic.org/\" target=\"_blank\">Free Cyber Clinic</a> and working as an officer in <a href=\"https://layer-zero.org\" target=\"_blank\">Layer Zero</a>.',
-        'Outside of school I also do photography and video production. I enjoy anime and am involved in the Las Vegas anime scene hosting events and panels at various conventions, cosplaying at events, and more! I can also be found playing music, cooking, hiking, and doing many other things as well. '
-    ]
-
     nameInterval: any;
 
     ngOnInit(): void
     {
         this.typing(this.name, this.fullName, 50);
-        this.typing(this.desc, this.fullDescription, 10);
+        this.typing(this.desc, this.fullDescription, 10, true);
     }
 
-    private typing(obj: { startText: string; }, finalText: string, interval: number)
+    private typing(obj: { startText: string; }, finalText: string, interval: number, finish: boolean = false)
     {
       const textArray = finalText.split('');
       let currentIndex = 0;
@@ -172,8 +165,9 @@ export class AppComponent {
           }
           else
           {
-              if(totalTime % 400 == 0)
+              if(totalTime % 400 == 0 && !finish)
               {
+
                   if(obj.startText[obj.startText.length - 1] == "_")
                   {
                       obj.startText = obj.startText.substring(0, obj.startText.length - 1);
@@ -186,10 +180,10 @@ export class AppComponent {
           }
           totalTime += interval;
 
-          // if(currentIndex == textArray.length)
-          // {
-          //     clearInterval(this.nameInterval);
-          // }
+          if(currentIndex == textArray.length && finish)
+          {
+              clearInterval(this.nameInterval);
+          }
       }, interval);
     }
 }
